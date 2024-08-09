09093ae204013ecf37fff89962c1df56b936488c
EC2 Default User <ec2-user@ip-172-31-28-234.ec2.internal>
2024-08-09 15:25:55 +0000

    Tabela contatos adicionada e migration criada

--- banco.sql
+++ banco.sql
@@ -63,6 +63,23 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
+CREATE TABLE IF NOT EXISTS `contatos` (
+  `idContatos` INT NOT NULL AUTO_INCREMENT,
+  `nomeContato` VARCHAR(255) NOT NULL,
+  `telefone` VARCHAR(20) NULL DEFAULT NULL,
+  `celular` VARCHAR(20) NOT NULL,
+  `clientes_id` INT NOT NULL,
+  PRIMARY KEY (`idContatos`),
+  INDEX `fk_contatos_clientes_idx` (`clientes_id` ASC),
+  CONSTRAINT `fk_contatos_clientes`
+    FOREIGN KEY (`clientes_id`)
+    REFERENCES `clientes` (`idClientes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET utfmb4 COLLATE utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `resets_de_senha` ( 
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(200) NOT NULL , 
